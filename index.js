e83e1ae3e4420206339d7f898e094d4035f7f70a
Arevik <arevarevyan@gmail.com>
2022-12-02 23:36:29 +0400

    change port

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/getthemagic/:id', (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
 })
