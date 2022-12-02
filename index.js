841dbfd4c2799b7c7a95a0cafbf04d54c74b993d
Arevik <arevarevyan@gmail.com>
2022-12-02 22:36:50 +0400

    initial

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-hello
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 5000;
+
+var magicData = {};
+
+fs.readFile('data.json', 'utf8', function(err, data){
+    magicData = JSON.parse(data); 
+});
+
+app.get('/getthemagic', (req, res) => {
+    res.json(magicData);
+})
+app.get('/getthemagic/:id', (req, res) => {
+    if (magicData[req.params.id]) {
+        res.json(magicData[req.params.id]);
+    }
+    else {
+        res.json({"error": "No data found."});
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
