dcf2933171d42ddc7c01dd49e14ffd79fd56ea7d
Arevik <arevarevyan@gmail.com>
2022-12-02 23:32:27 +0400

    add logs

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ fs.readFile('data.json', 'utf8', function(err, data){
     magicData = JSON.parse(data); 
 });
 
+app.get('/', (req, res) => {
+    console.log("/");
+    res.sendStatus(200)
+  })
+
 app.get('/getthemagic', (req, res) => {
+    console.log("/getthemagic");
+
     res.json(magicData);
 })
 app.get('/getthemagic/:id', (req, res) => {
+    console.log("/getthemagic/" + req.params.id);
+
     if (magicData[req.params.id]) {
         res.json(magicData[req.params.id]);
     }
