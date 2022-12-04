e2a2a6e248003fc0c02800413b6bbfd34c3dc6e0
Arevik Arevyan <arevarevyan@gmail.com>
2022-12-04 04:12:59 +0400

    add to db

---
+++ inBrowser.js
@@ -0,0 +1,15 @@
+var tag = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+fetch('https://magic-iq8u.onrender.com/getthemagic/' + tag)
+  .then((response) => response.json())
+  .then((data) => {
+    var questionStyle = 'color: black;';
+    var wrongAnswerStyle = 'color: red; margin-left:20px;';
+    var correctAnswerStyle = 'color:green; font-weight: bold; margin-left:20px;';
+    for (var i of data) {
+      console.log(`%c ${i.question} `, questionStyle);
+      for (var j of i.answers) {
+        if (j.isCorrect) console.log(`%c ${j.text} `, correctAnswerStyle);
+        else console.log(`%c ${j.text} `, wrongAnswerStyle);
+      }
+    }
+  });
